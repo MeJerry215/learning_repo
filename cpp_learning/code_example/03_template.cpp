33311512c1fa071049ab7cf70eda8571c772c7e0
chenugray <chenugray@outlook.com>
2022-04-28 23:44:37 +0800

    add something

--- cpp_learning/code_example/03_template.cpp
+++ cpp_learning/code_example/03_template.cpp
@@ -3,21 +3,40 @@
 template<typename T>
 class String {
 public:
-    // String() {
-        // std::cout << "string construct" << std::endl;
-    // }
+    String() {
+        std::cout << "string construct" << std::endl;
+    }
 
-    String();
 };
 
+template<>
+class String<float> {
+public:
+    String() {
+        std::cout << "float construct " << std::endl;
+    }
+};
 
-template<typename T> String<T>::String() {
-    std::cout << "string construct" << std::endl;
-}
+template<typename T, int size>
+class Buffer {
+    T v[size];
+    const static int sz = size;
+public:
+    Buffer() {
+        std::cout << "size " << sz << std::endl;
+    }
+};
+
+/*
+specialization
+*/
+
 
 
 int main(int argc, char const *argv[])
 {
-    String<int> s;
+    String<int> s1;
+    Buffer<float, 2> b;
+    String<float> s2;
     return 0;
 }
--- cpp_learning/code_example/include/common.h
+++ cpp_learning/code_example/include/common.h
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <stdio.h>
-#include <vector>
+#include <vector>   // 数组
+#include <list>     // 双向链表
+#include <deque>    // 双端队列
+#include <queue>    // 队列
+#include <stack>    // 堆栈
+#include <map>      // 关联数组
+#include <set>      // 集合
+#include <utility>  // 运算符和对偶
+#include <functional>   // 函数对象
+#include <memory>   // 容器用的分配器
+#include <ctime>    // c风格时间和日期
+#include <iterator> // 迭代器
+#include <algorithm>    //  通用算法
+#include <exception>    // 异常
+#include <string>   // 字符串
+#include <cassert>  // assert
+#include <limits>   // 数值范围
+
+#include <cstdlib>
+
 
 #define SIZE(arr, T) (sizeof(arr)/sizeof(T))
 
