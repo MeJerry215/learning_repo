a966d9b4c2a64436d87113518d4d1e6274d77018
chenugray <chenugray@outlook.com>
2022-09-19 23:18:48 +0800

    update 0543

--- leetcode/0543_diameter-of-binary-tree.cpp
+++ leetcode/0543_diameter-of-binary-tree.cpp
@@ -1,17 +1,22 @@
 #include "common.h"
 
 class Solution {
-    int ans;
-    int depth(TreeNode* rt){
-        if (rt == NULL) {
-            return 0; // 访问到空节点了，返回0
-        }
-        int L = depth(rt->left); // 左儿子为根的子树的深度
-        int R = depth(rt->right); // 右儿子为根的子树的深度
-        ans = max(ans, L + R + 1); // 计算d_node即L+R+1 并更新ans
-        return max(L, R) + 1; // 返回该节点为根的子树的深度
-    }
 public:
+    int ans;
+
+    int depth(TreeNode *node) {
+        if (!node) {
+            return 0;
+        }
+        // 左树高度
+        int l = depth(node->left);
+        // 右树高度
+        int r = depth(node->right);
+        // 左右树节点相加, 加上本节点个数
+        ans = max(ans, l + r + 1);
+        return max(l, r) + 1;
+    }
+
     int diameterOfBinaryTree(TreeNode* root) {
         ans = 1;
         depth(root);
