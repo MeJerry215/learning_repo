c4d323347a269172a56eee896e3f65ae25856705
chenugray <chenugray@outlook.com>
2022-09-20 23:35:43 +0800

    @todo kmp

---
+++ leetcode/0028_find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,9 @@
+#include "common.h"
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int pos = haystack.find(needle);
+        return pos == string::npos ? -1: pos;
+    }
+};
