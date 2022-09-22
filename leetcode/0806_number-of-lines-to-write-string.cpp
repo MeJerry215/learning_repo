2966870c4cdc5d60f4fdc23a78d0f28d3b1cd7ee
JerryChenug <chenugray@outlook.com>
2022-09-22 20:35:04 +0800

    update

---
+++ leetcode/0806_number-of-lines-to-write-string.cpp
@@ -0,0 +1,27 @@
+#include "common.h"
+
+
+class Solution
+{
+public:
+    vector<int> numberOfLines(vector<int> &widths, string s)
+    {
+        int cur = 0;
+        int line = 1;
+        for (const auto c : s)
+        {
+            int width = widths[c - 'a'];
+            cur = width + cur;
+            if (cur <= 100)
+            {
+                continue;
+            }
+            else
+            {
+                cur = width;
+                line += 1;
+            }
+        }
+        return {line, cur};
+    }
+};
