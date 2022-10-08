f47c6f15025fad294b7c22ca1299cdf0c7e686dd
chenugray <chenugray@outlook.com>
2022-10-08 23:35:23 +0800

    update robeber house 0198 0213 0337

--- leetcode/0337_house-robber-iii.cpp
+++ leetcode/0337_house-robber-iii.cpp
@@ -1,20 +1,30 @@
 #include "common.h"
+
 /*
 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+
 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+
 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
 
+ 
+
 示例 1:
+
+
+
 输入: root = [3,2,3,null,3,null,1]
 输出: 7 
 解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7
 示例 2:
 
 
+
 输入: root = [3,4,5,1,3,null,1]
 输出: 9
 解释: 小偷一晚能够盗取的最高金额 4 + 5 = 9
  
+
 提示：
 
 树的节点数在 [1, 104] 范围内
@@ -24,25 +34,31 @@
 链接：https://leetcode.cn/problems/house-robber-iii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int robRange(vector<int>& nums, int start, int end) {
-        int first = nums[start], second = max(nums[start], nums[start + 1]);
-        for (int i = start + 2; i <= end; i++) {
-            int temp = second;
-            second = max(first + nums[i], second);
-            first = temp;
-        }
-        return second;
+    pair<int,int> rob_helper(TreeNode* node) {
+        if (!node) return {0, 0};
+        auto lhs = rob_helper(node->left);
+        auto rhs = rob_helper(node->right);
+        int take_from_this_node = lhs.second + rhs.second + node->val;
+        int untake_this_node = max(lhs.first, lhs.second) + max(rhs.first, rhs.second);
+        return make_pair(take_from_this_node, untake_this_node);
     }
 
-    int rob(vector<int>& nums) {
-        int length = nums.size();
-        if (length == 1) {
-            return nums[0];
-        } else if (length == 2) {
-            return max(nums[0], nums[1]);
-        }
-        return max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    int rob(TreeNode* root) {
+        auto res = rob_helper(root);
+        return max(res.first, res.second);
     }
 };
