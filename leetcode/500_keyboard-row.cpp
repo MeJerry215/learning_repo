b3ea8428cf57ca547808e3e8d2e6fd82bd8d1267
chenugray <chenugray@outlook.com>
2022-10-09 21:40:00 +0800

    update 500

--- leetcode/500_keyboard-row.cpp
+++ leetcode/500_keyboard-row.cpp
@@ -4,6 +4,30 @@
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-
+        vector<string> ans;
+        string rowIdx = "12210111011122000010020202";
+        for (auto & word : words) {
+            bool is_valid = true;
+            char idx = rowIdx[tolower(word[0]) - 'a'];
+            for (int i = 1; i < word.size(); ++i) {
+                if(rowIdx[tolower(word[i]) - 'a'] != idx) {
+                    is_valid = false;
+                    break;
+                }
+            }
+            if (is_valid) {
+                ans.emplace_back(word);
+            }
+        }
+        return ans;
     }
 };
+
+void TestSolution() {
+    Solution s;
+    string tmp = "qwertyuiop";
+        for (int c : tmp) {
+            cout << c << " ";
+        }
+        cout << endl; 
+}
