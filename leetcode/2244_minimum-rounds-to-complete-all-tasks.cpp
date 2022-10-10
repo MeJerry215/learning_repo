58f17e2a1122b4a2d920661e0008ec82b39a26ea
chenugray <chenugray@outlook.com>
2022-10-10 21:56:47 +0800

    update 2244 one word: 只有当iter 为1的时候才无解

---
+++ leetcode/2244_minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+
+class Solution
+{
+public:
+    int minimumRounds(vector<int> &tasks)
+    {
+        unordered_map<int, int> task_count;
+        for (auto task : tasks)
+        {
+            task_count[task]++;
+        }
+        int res = 0;
+        for (auto iter : task_count)
+        {
+            if (iter.second % 3 == 1 && iter.second / 3 == 0) return -1;
+            res = res + iter.second / 3 + (iter.second % 3 > 0);
+        }
+        return res;
+    }
+};
