d5fcc8abd7c98d1639fca87380d794e1c4bc5367
chenugray <chenugray@outlook.com>
2022-10-11 21:56:40 +0800

    update 0829, one word: 15 = 15 % 1 == 0,  (15 - 1) % 2 == 0 (7 + 7 + 1), (15 - 1 - 2) % 3 == 0 (4 + 4 + 4 + 1 + 2), (15 - 1 - 2 - 3) % 4 != 0 , (15 - 1 - 2 - 3 - 4) % 5 == 0 (1 + 1 + 1 + 1 + 1 +    1 + 2 + 3 + 4)

--- leetcode/0829_consecutive-numbers-sum.cpp
+++ leetcode/0829_consecutive-numbers-sum.cpp
@@ -1,10 +1,9 @@
 #include "common.h"
 
-
 /*
-给定一个正整数 n，返回 连续正整数满足所有数字之和为 n 的组数 。 
+给定一个正整数 n，返回 连续正整数满足所有数字之和为 n 的组数 。
 
- 
+
 
 示例 1:
 
@@ -21,50 +20,41 @@
 输入: n = 15
 输出: 4
 解释: 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
- 
+
 
 提示:
 
 1 <= n <= 109​​​​​​​
 
 */
-class Solution {
+class Solution
+{
 public:
-    int consecutiveNumbersSum(int n) {
-        int ans = 0;
-        int bound = 2 * n;
-        for (int k = 1; k * (k + 1) <= bound; k++) {
-            if (isKConsecutive(n, k)) {
-                ans++;
-            }
-        }
-        return ans;
-    }
-  
-    bool isKConsecutive(int n, int k) {
-        if (k % 2 == 1) {
-            return n % k == 0;
-        } else {
-            return n % k != 0 && 2 * n % k == 0;
-        }
+    int consecutiveNumbersSum(int N)
+    {
+        int res = 0;
+        for (int i = 1; N > 0; N -= i++)
+            res += (N % i == 0);
+        return res;
     }
 
-
-    int consecutiveNumbersSum_v1(int n) {
+    int consecutiveNumbersSum_v1(int n)
+    {
         int res = 0;
         int sum = 0;
         int low = 1, high = 1;
-        for(; high <= n; high++) {
+        for (; high <= n; high++)
+        {
             sum += high;
             if (sum == n)
-                res ++;
-            while(sum >= n && low < high) {
+                res++;
+            while (sum >= n && low < high)
+            {
                 sum -= low++;
                 if (sum == n)
-                    res ++;
+                    res++;
             }
         }
         return res;
     }
 };
-
