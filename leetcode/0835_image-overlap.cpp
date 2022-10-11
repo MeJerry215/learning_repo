a373061d4f02192cd437ac31abd8785fc763b836
chenugray <chenugray@outlook.com>
2022-10-12 01:13:33 +0800

    updatre 0835

--- leetcode/0835_image-overlap.cpp
+++ leetcode/0835_image-overlap.cpp
@@ -20,9 +20,35 @@ public:
             }
         } else {
             int x_iter = n;
+ class Solution {
+public:
+    int compare_overlap(vector<vector<int>>& img1, vector<vector<int>>& img2, int x_offset, int y_offset) {
+        /* 这里只移动 img1, 所以img1的索引需要被重新计算，而img2 是不需要被重新计算的 */
+        int overlap_num = 0;
+        int n = img1.size();
+        int y_iter = n + y_offset;
+
+        if (x_offset <= 0) {
+            int x_iter = n + x_offset;
+            for(int i = 0; i < x_iter; i++) {
+                for(int j = 0; j < y_iter; j++) {
+                    if (img2[j][i] == 1 && img2[j][i] == img1[j - y_offset][i - x_offset]) {
+                        overlap_num ++;
+                    }
+                }
+            }
+        } else {
+            int x_iter = n;
+            if (x_offset == n - 1 && y_offset == - (n - 1)) {
+                cout << "x_iter " << x_iter << " " << y_iter << " " << x_offset << " " << y_offset <<   endl;
+            } 
             for(int i = x_offset; i < x_iter; i++) {
                 for(int j = 0; j < y_iter; j++) {
-                    if (img2[j][i] == 1 && img2[j][i] == img1[j - y_offset][x_offset]) {
+                    if (i == 4 && j == 0 && x_offset == 4 && y_offset == -4) {
+                        cout << "img2[j][i] " << img2[j][i] << " img1[j - y_offset][i] " << img1[j - y_offset][i] << endl;
+                        cout << i << " " << j << " " << j - y_offset << endl;
+                    }
+                    if (img2[j][i] == 1 && img2[j][i] == img1[j - y_offset][i]) {
                         overlap_num ++;
                     }
                 }
@@ -39,7 +65,6 @@ public:
         img1 往下右偏移 等价于img2往上左偏移， 所以这里只考虑往上左偏移的情况，需要交换重复计算,
         同理 img1 往下左，等价img2往上右偏移。
          */
-        cout << - (n - 1) << endl;
         for(int x_offset = - (n - 1); x_offset <= n - 1; x_offset++) {
             for(int y_offset = -(n - 1); y_offset <= 0; y_offset++) {
                 res = max(res, compare_overlap(img1, img2, x_offset, y_offset));
@@ -50,15 +75,14 @@ public:
     }
 };
 
-
 void TestSolution() {
     Solution s;
     vector<vector<int>> img1 = {
-        {1,1,0},{0,1,0},{0,1,0}
+        {0,0,0,0,1},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}
     };
     vector<vector<int>> img2 = {
-        {0,0,0},{0,1,1},{0,0,1}
+        {0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{1,0,0,0,0}
     };
     cout << s.largestOverlap(img1, img2) << endl;
-}
+};
 
