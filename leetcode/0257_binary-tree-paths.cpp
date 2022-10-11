120fb933c29d5c3d8cbe21cab2a42304f7046b94
chenugray <chenugray@outlook.com>
2022-10-11 20:57:19 +0800

    update 0257

--- leetcode/0257_binary-tree-paths.cpp
+++ leetcode/0257_binary-tree-paths.cpp
@@ -1,8 +1,33 @@
 #include "common.h"
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> res;
+        function<void(TreeNode *, string)> dfs = [&](TreeNode *node, string path)
+        {
+            if (!node)
+                return;
+            
+            if (path.size())
+            {
+                path = path + "->" + to_string(node->val);
+            }
+            else
+            {
+                path = to_string(node->val);
+            }
+            if (!node->left && !node->right)
+            {
+                res.push_back(path);
+            }
+            dfs(node->left, path);
+            dfs(node->right, path);
+        };
+        string path_str = "";
+        dfs(root, path_str);
+        return res;
     }
 };
