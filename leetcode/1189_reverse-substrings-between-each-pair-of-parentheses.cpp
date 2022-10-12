b231864d96e75c8e0e343ddab4c08dc485771178
chenugray <chenugray@outlook.com>
2022-10-13 01:02:48 +0800

    update 0063, 0757, 0813, 0839, 0982, 1189, 1192, 1654, 1748, 2180, 2215, 2262

---
+++ leetcode/1189_reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,44 @@
+#include "common.h"
+
+/*
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+ 
+
+Example 1:
+
+Input: s = "(abcd)"
+Output: "dcba"
+Example 2:
+
+Input: s = "(u(love)i)"
+Output: "iloveu"
+Explanation: The substring "love" is reversed first, then the whole string is reversed.
+Example 3:
+
+Input: s = "(ed(et(oc))el)"
+Output: "leetcode"
+Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s only contains lower case English characters and parentheses.
+It is guaranteed that all parentheses are balanced.
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/reverse-substrings-between-each-pair-of-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+
+    }
+};
