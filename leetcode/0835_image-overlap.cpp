6fff821f5210995bd5911e2f5f485250629ce9ef
chenugray <chenugray@outlook.com>
2022-10-12 20:51:17 +0800

    update 0835 , one word, 我以为可能会有巧妙地方法，但是好像只能暴力

--- leetcode/0835_image-overlap.cpp
+++ leetcode/0835_image-overlap.cpp
@@ -1,61 +1,49 @@
 #include "common.h"
 
 
-class Solution {
+class Solution
+{
 
 public:
-    int compare_overlap(vector<vector<int>>& img1, vector<vector<int>>& img2, int x_offset, int y_offset) {
-        /* 这里只移动 img1, 所以img1的索引需要被重新计算，而img2 是不需要被重新计算的 */
-        int overlap_num = 0;
+    int compare_overlap(vector<vector<int>> &img1, vector<vector<int>> &img2, int x_offset, int y_offset)
+    {
+        int res = 0;
         int n = img1.size();
-        int y_iter = n + y_offset;
-
-        if (x_offset <= 0) {
-            int x_iter = n + x_offset;
-            for(int i = 0; i < x_iter; i++) {
-                for(int j = 0; j < y_iter; j++) {
-                    if (img2[j][i] == 1 && img2[j][i] == img1[j - y_offset][i - x_offset]) {
-                        overlap_num ++;
-                    }
-                }
-            }
-        } else {
-            int x_iter = n;
-            if (x_offset == n - 1 && y_offset == - (n - 1)) {
-                cout << "x_iter " << x_iter << " " << y_iter << " " << x_offset << " " << y_offset <<   endl;
-            } 
-            for(int i = x_offset; i < x_iter; i++) {
-                for(int j = 0; j < y_iter; j++) {
-                    if (i == 4 && j == 0 && x_offset == 4 && y_offset == -4) {
-                        cout << "img2[j][i] " << img2[j][i] << " img1[j - y_offset][i] " << img1[j - y_offset][i] << endl;
-                        cout << i << " " << j << " " << j - y_offset << endl;
-                    }
-                    if (img2[j][i] == 1 && img2[j][i] == img1[j - y_offset][i]) {
-                        overlap_num ++;
-                    }
+        int x_low = max(0, x_offset), x_high = min(n, n + x_offset);
+        int y_low = max(0, y_offset), y_high = min(n, n + y_offset);
+        for (int i = y_low; i < y_high; i++)
+        {
+            for (int j = x_low; j < x_high; j++)
+            {
+                if (img2[i][j] == 1 && img2[i][j] == img1[i - y_offset][j - x_offset])
+                {
+                    res++;
                 }
             }
         }
-        return overlap_num;
+        return res;
     }
 
-
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int res = 0;
+    int largestOverlap(vector<vector<int>> &img1, vector<vector<int>> &img2)
+    {
         int n = img1.size();
+        int res = 0;
         /*
-        img1 往下右偏移 等价于img2往上左偏移， 所以这里只考虑往上左偏移的情况，需要交换重复计算,
-        同理 img1 往下左，等价img2往上右偏移。
+        总是移动nums1, nums2固定不动。使用x_offset, y_offset表示nums1的偏移。
+        x_offset = -1, y_offset = -1 表示nums1 向左上各偏移1个单位。
+        而nums1可以偏移的的范围至少要保证一个相交点存在所以偏移范围为1 - n < x_offset, y_offset < n - 1
          */
-        for(int x_offset = - (n - 1); x_offset <= n - 1; x_offset++) {
-            for(int y_offset = -(n - 1); y_offset <= 0; y_offset++) {
+        for (int y_offset = 1 - n; y_offset <= n - 1; y_offset++)
+        {
+            for (int x_offset = 1 - n; x_offset <= n - 1; x_offset++)
+            {
                 res = max(res, compare_overlap(img1, img2, x_offset, y_offset));
-                res = max(res, compare_overlap(img2, img1, x_offset, y_offset));
             }
         }
         return res;
     }
 };
+
 
 void TestSolution() {
     Solution s;
