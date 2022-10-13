e6e3f851a69ed110073ddf414e3c85b5c75ff09d
chenugray <chenugray@outlook.com>
2022-10-14 00:49:07 +0800

    update 0840

---
+++ leetcode/0840_magic-squares-in-grid.cpp
@@ -0,0 +1,35 @@
+#include "common.h"
+
+/*
+3 x 3 的幻方是一个填充有 从 1 到 9  的不同数字的 3 x 3 矩阵，其中每行，每列以及两条对角线上的各数之和都相等。
+给定一个由整数组成的row x col 的 grid，其中有多少个 3 × 3 的 “幻方” 子矩阵？（每个子矩阵都是连续的）。
+示例 1：
+
+输入: grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]
+输出: 1
+解释: 
+下面的子矩阵是一个 3 x 3 的幻方：
+而这一个不是：
+总的来说，在本示例所给定的矩阵中只有一个 3 x 3 的幻方子矩阵。
+示例 2:
+输出: grid = [[8]]
+输入: 0
+
+提示:
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 10
+0 <= grid[i][j] <= 15
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/magic-squares-in-grid
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+
+    }
+};
