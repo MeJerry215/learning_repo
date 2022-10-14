cb6b1563e1e2da9b2a3d5113b8980a51c8bd68b6
chenugray <chenugray@outlook.com>
2022-10-15 02:34:33 +0800

    update 1441

--- leetcode/1441_build-an-array-with-stack-operations.cpp
+++ leetcode/1441_build-an-array-with-stack-operations.cpp
@@ -49,8 +49,8 @@ public:
                     res.push_back("Pop");
                     offset++;
                 }
-                offset --;
             }
+            res.push_back("Push");
         }
         return res;
     }
