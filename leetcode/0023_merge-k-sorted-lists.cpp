7fb056debf9720829b77f4a31d335a67544150cf
chenugray <chenugray@outlook.com>
2022-10-15 20:49:04 +0800

    update 0023, uhow to using priority_queue to construct a min head

--- leetcode/0023_merge-k-sorted-lists.cpp
+++ leetcode/0023_merge-k-sorted-lists.cpp
@@ -3,33 +3,33 @@
 
 class Solution {
 public:
-    ListNode* mergeKLists_2(vector<ListNode*>& lists) {
+    struct compare {
+        bool operator()(const ListNode* lhs, const ListNode* rhs) {
+            return lhs->val > rhs->val;
+        }
+    };
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode *head = new ListNode(0);
         ListNode *cur = head;
-        while (lists.size() > 0)
-        {
-            ListNode *min_node = lists[0];
-            int k = 0;
-            if (min_node) {
-            for(int i = 1; i < lists.size(); i++) {
-                if (lists[i] && min_node->val > lists[i]->val) {
-                    k = i;
-                    min_node = lists[i];
-                }
-            }
-            cur->next = min_node;
-            min_node = min_node->next;
-            cur = cur->next;
-            cur->next = nullptr;
-            lists[k] = min_node;
-
-            }
-            if (min_node == nullptr) {
-                lists.erase(lists.begin() + k);
-            }
+        priority_queue<ListNode*, vector<ListNode*>, compare> merge_lists;
+        for(ListNode* node: lists) {
+            if (node)
+                merge_lists.push(node);
         }
-        
+        while(merge_lists.size()) {
+            ListNode *node = merge_lists.top();
+            merge_lists.pop();
+            cur->next = node;
+            cur = node;
+            node = node->next;
+            if (node) {
+                merge_lists.push(node);
+            }
+            cur->next = nullptr;
+        }
+
         return head->next;
     }
-
 };
