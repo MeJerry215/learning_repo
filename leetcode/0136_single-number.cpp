a40d8c1b4165c961df2621d3b69164a189b0dd98
chenugray <chenugray@outlook.com>
2022-10-20 21:41:17 +0800

    update 0136

--- leetcode/0136_single-number.cpp
+++ leetcode/0136_single-number.cpp
@@ -3,6 +3,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor = 0;
+        int xor_val = 0;
+        for(auto num: nums) xor_val ^= num;
+        return xor_val;
     }
 };
