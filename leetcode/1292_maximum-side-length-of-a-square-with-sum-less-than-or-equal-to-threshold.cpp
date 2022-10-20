649010f78726fb42441d475ed087c4c432b91c73
chenugray <chenugray@outlook.com>
2022-10-20 21:30:15 +0800

    update 1292, one word: 二维矩阵求前缀和, P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + A[i][j], 求给定区域 大小和为P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1], 此题可以用二分搜索加，以及直接枚举进行加速

--- leetcode/1292_maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ leetcode/1292_maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,50 +1,44 @@
 #include "common.h"
 
-class Solution
-{
+class Solution {
 public:
-    bool isValid(vector<vector<int>> &mat, int x, int y, int win_size, int threshold)
-    {
-        int sum = 0;
-        for (int i = x; i < x + win_size; i++) {
-            for (int j = y; j < y + win_size; j++) {
-                sum += mat[i][j];
-                if (sum > threshold)
-                    return false;
-            }
-        }
-        return true;
+    int getRect(const vector<vector<int>>& P, int x1, int y1, int x2, int y2) {
+        return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1];
     }
 
-    int maxSideLength(vector<vector<int>> &mat, int threshold)
-    {
-        int m = mat.size();
-        int n = mat[0].size();
-        // 用来加速搜索，如果在windows_size较小的时候, 窗口内找不到满足的，则更大的windows更加不会满足
-        vector<vector<bool>> visited(m, vector<bool>(n, true));
-        int win_size = 0;
-        // win_size最大不会超过min(m, n)
-        for (int i = 1; i <= min(m, n); i++) {
-            bool isFind = false;
-            for (int j = 0; j < m - i + 1; j++) {
-                if (isFind)
-                    break;
-                for (int k = 0; k < n - i + 1; k++) {
-                    if (!visited[j][k])
-                        continue;
-                    if (isValid(mat, j, k, i, threshold)) {
-                        isFind = true;
-                        win_size = i;
-                        break;
-                    } else {
-                        visited[j][k] = false;
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> P(m + 1, vector<int>(n + 1));
+        /*
+        二维矩阵求前缀和
+        A[i][j] = P[i][j] - P[i - 1][j] - P[i][j - 1] + P[i - 1][j - 1]
+        P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + A[i][j]
+        */
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + mat[i - 1][j - 1];
+            }
+        }
+
+        int l = 1, r = min(m, n), ans = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            bool find = false;
+            for (int i = 1; i <= m - mid + 1; ++i) {
+                for (int j = 1; j <= n - mid + 1; ++j) {
+                    if (getRect(P, i, j, i + mid - 1, j + mid - 1) <= threshold) {
+                        find = true;
                     }
                 }
             }
-            // 如果整个循环内部都找不到一个，则更大的窗口也不会满足
-            if (!isFind)
-                break;
+            if (find) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
         }
-        return win_size;
+        return ans;
     }
 };
