2cfd98062cb4fea20cc0a596b47853720d9e7e48
chenugray <chenugray@outlook.com>
2022-10-20 21:38:57 +0800

    update 2090, one word: 滑动窗口求和平均

---
+++ leetcode/2090_k-radius-subarray-averages.cpp
@@ -0,0 +1,23 @@
+#include "common.h"
+
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+    if (k == 0) return nums;
+    vector<int> res(nums.size(), -1);
+    if (2 * k + 1 > nums.size()) return res;
+    int sum = 0;
+
+    for(int i = 0; i < 2 * k + 1; i++) {
+      sum += nums[i];
+    }
+    cout << sum << endl;
+    res[k] = sum / (2 * k + 1);
+    for(int i = k + 1; i < nums.size() - k; i++) {
+        sum -= nums[i - k - 1];
+        sum += nums[i + k];
+        res[i] = sum / (2 * k + 1);
+    }
+    return res;
+  }
+};
