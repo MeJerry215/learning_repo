4527969ee5352794611e4f65228589d061190385
chenugray <chenugray@outlook.com>
2022-10-25 21:48:01 +0800

    update 1540, one word: 由于字符只有26个，所以可以记录当前字符的偏移以及次数，重新算出要offset多少次

---
+++ leetcode/1540_can-convert-string-in-k-moves.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+
+
+class Solution
+{
+public:
+    bool canConvertString(string s, string t, int k)
+    {
+        if (s.size() != t.size())
+            return false;
+        vector<int> counts(26, 0);
+        for (int i = 0; i < s.size(); i++)
+        {
+            int diff = t[i] - s[i];
+            if (diff == 0)
+                continue;
+            if (diff < 0)
+                diff += 26;
+            if (diff + counts[diff] * 26 > k)
+                return false;
+            counts[diff]++;
+        }
+        return true;
+    }
+};
