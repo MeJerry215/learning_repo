615c11f514db485c326b4ec3c4ccad3593031fce
chenugray <chenugray@outlook.com>
2022-10-28 00:00:41 +0800

    update for 2022/10/29, todo

---
+++ leetcode/todos/1305.两棵二叉搜索树中的所有元素.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=1305 lang=cpp
+ *
+ * [1305] 两棵二叉搜索树中的所有元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+给你 root1 和 root2 这两棵二叉搜索树。请你返回一个列表，其中包含 两棵树 中的所有整数并按 升序 排序。.
+
+ 
+
+示例 1：
+
+
+
+输入：root1 = [2,1,4], root2 = [1,0,3]
+输出：[0,1,1,2,3,4]
+示例 2：
+
+
+
+输入：root1 = [1,null,8], root2 = [8,1]
+输出：[1,1,8,8]
+ 
+
+提示：
+
+每棵树的节点数在 [0, 5000] 范围内
+-105 <= Node.val <= 105
+ */
+class Solution {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+
+    }
+};
+// @lc code=end
+
---
+++ leetcode/todos/234.回文链表.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=cpp
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,2,2,1]
+输出：true
+示例 2：
+
+
+输入：head = [1,2]
+输出：false
+ 
+
+提示：
+
+链表中节点数目在范围[1, 105] 内
+0 <= Node.val <= 9
+ 
+
+进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+    }
+};
+// @lc code=end
+
