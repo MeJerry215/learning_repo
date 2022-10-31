10ff39d8f3c5566c3c1cc979060a4a0f62512c74
chenugray <chenugray@outlook.com>
2022-11-01 00:55:46 +0800

    update todos

---
+++ leetcode/todos/2433.找出前缀异或的原始数组.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2433 lang=cpp
+ *
+ * [2433] 找出前缀异或的原始数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        
+    }
+};
+// @lc code=end
+
