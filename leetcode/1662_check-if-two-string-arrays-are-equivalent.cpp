d8f6c3237fda838542f8707a2de2ead550b268c3
chenugray <chenugray@outlook.com>
2022-11-01 00:50:33 +0800

    update 1662

--- leetcode/1662_check-if-two-string-arrays-are-equivalent.cpp
+++ leetcode/1662_check-if-two-string-arrays-are-equivalent.cpp
@@ -7,20 +7,24 @@ public:
     {
         int i = 0, k = 0, l = 0;
         while(i < word1.size()) {
-            for(int j = 0; j < word1[0].size(); j ++) {
-                if (k < word2.size() && l < word2[0].size()) {
+            for(int j = 0; j < word1[i].size(); j ++) {
+                if (k < word2.size() && l < word2[k].size()) {
                     if (word1[i][j] == word2[k][l]) {
-
+                        l++;
                     } else {
-
+                        return false;
                     }
                 } else if (k < word2.size()) {
                     k ++;
+                    l = 0;
+                    j --;
                 } else if (k == word2.size()) {
                     return false;
                 }
             }
+            i++;
         }
-        return true;
+        if (k == word2.size() - 1 && l == word2[k].size()) return true;
+        return false;
     }
 };
