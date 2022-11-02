887c8aa3b4c77066602d836a0db69511bd599c58
chenugray <chenugray@outlook.com>
2022-11-03 00:43:57 +0800

    update todos

---
+++ leetcode/todos/938.二叉搜索树的范围和.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=938 lang=cpp
+ *
+ * [938] 二叉搜索树的范围和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+
+ 
+
+示例 1：
+
+
+输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+输出：32
+示例 2：
+
+
+输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+输出：23
+ 
+
+提示：
+
+树中节点数目在范围 [1, 2 * 104] 内
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+所有 Node.val 互不相同
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+
+    }
+};
+// @lc code=end
+
