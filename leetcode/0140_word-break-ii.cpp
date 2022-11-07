dcaf3a32a405410985a2d40ccc628cf075ea205b
chenugray <chenugray@outlook.com>
2022-11-07 22:17:01 +0800

    update 0140, one word: bfs

---
+++ leetcode/0140_word-break-ii.cpp
@@ -0,0 +1,44 @@
+#include "common.h"
+
+/*
+给定一个字符串 s 和一个字符串字典 wordDict ，在字符串 s 中增加空格来构建一个句子，使得句子中所有的单词都在词典中。以任意顺序 返回所有这些可能的句子。
+
+注意：词典中的同一个单词可能在分段中被重复使用多次。
+
+ 
+
+示例 1：
+
+输入:s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+输出:["cats and dog","cat sand dog"]
+示例 2：
+
+输入:s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+输出:["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+解释: 注意你可以重复使用字典中的单词。
+示例 3：
+
+输入:s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+输出:[]
+ 
+
+提示：
+
+1 <= s.length <= 20
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 10
+s 和 wordDict[i] 仅有小写英文字母组成
+wordDict 中所有字符串都 不同
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/word-break-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution
+{
+public:
+    vector<string> wordBreak(string s, vector<string> &wordDict)
+    {
+    }
+};
