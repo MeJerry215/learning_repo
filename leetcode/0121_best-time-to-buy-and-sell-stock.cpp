c27bb51430df95ac446d51d7d3e08d32af790cd3
chenugray <chenugray@outlook.com>
2022-11-09 21:53:46 +0800

    update 0121, one word sell

--- leetcode/0121_best-time-to-buy-and-sell-stock.cpp
+++ leetcode/0121_best-time-to-buy-and-sell-stock.cpp
@@ -1,8 +1,7 @@
 #include "common.h"
 
-
 /*
-给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 
@@ -16,7 +15,7 @@
 输入：prices = [7,6,4,3,1]
 输出：0
 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
- 
+
 
 提示：
 
@@ -28,15 +27,45 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-class Solution {
+class Solution
+{
 public:
-    int maxProfit_v1(vector<int>& prices) {
+    int maxProfit_v1(vector<int> &prices)
+    {
         int max_val = 0;
         int res = 0;
-        for(int i = prices.size() - 1; i >=0; i--) {
+        for (int i = prices.size() - 1; i >= 0; i--)
+        {
             res = max(res, max_val - prices[i]);
             max_val = max(max_val, prices[i]);
         }
         return res;
+    }
+
+    int maxProfit_v2(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> profits(n, vector<int>(2, 0));
+        profits[0][0] = 0;
+        profits[0][1] = -prices[0];
+        int p1 = 0, p2 = -prices[0];
+        for (int i = 1; i < n; i++)
+        {
+            profits[i][0] = max(profits[i - 1][0], profits[i - 1][1] + prices[i]);
+            profits[i][1] = max(profits[i - 1][1], -prices[i]);
+        }
+        return profits[n - 1][0];
+    }
+
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        int p1 = 0, p2 = -prices[0];
+        for (int i = 1; i < n; i++)
+        {
+            p1 = max(p1, p2 + prices[i]);
+            p2 = max(p2, -prices[i]);
+        }
+        return p1;
     }
 };
