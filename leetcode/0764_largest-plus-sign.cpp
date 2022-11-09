25b444024b52c6ef6614c3f111f6285535186fcd
chenugray <chenugray@outlook.com>
2022-11-09 21:48:45 +0800

    update 0764, one word: dp 4n^ 2

--- leetcode/0764_largest-plus-sign.cpp
+++ leetcode/0764_largest-plus-sign.cpp
@@ -1,13 +1,9 @@
 #include "common.h"
 
 /*
-在一个 n x n 的矩阵 grid 中，除了在数组 mines 中给出的元素为 0，其他每个元素都为 1。mines[i] = [xi, yi]表示 grid[xi][yi] == 0
-
-返回  grid 中包含 1 的最大的 轴对齐 加号标志的阶数 。如果未找到加号标志，则返回 0 。
-
-一个 k 阶由 1 组成的 “轴对称”加号标志 具有中心网格 grid[r][c] == 1 ，以及4个从中心向上、向下、向左、向右延伸，长度为 k-1，由 1 组成的臂。注意，只有加号标志的所有网格要求为 1 ，别的网格可能为 0 也可能为 1 。
-
- 
+在一个 n x n 的矩阵grid中，除了在数组mines中给出的元素为0，其他每个元素都为1。mines[i] = [xi, yi]表示grid[xi][yi] == 0
+返回 grid 中包含1的最大的 轴对齐 加号标志的阶数 。如果未找到加号标志，则返回 0 。
+一个k阶由1组成的 “轴对称”加号标志 具有中心网格grid[r][c] == 1，以及4个从中心向上、向下、向左、向右延伸，长度为k-1，由1组成的臂。注意，只有加号标志的所有网格要求为 1 ，别的网格可能为 0 也可能为 1 。
 
 示例 1：
 
@@ -23,23 +19,101 @@
 输入: n = 1, mines = [[0, 0]]
 输出: 0
 解释: 没有加号标志，返回 0 。
- 
+
 
 提示：
 
 1 <= n <= 500
 1 <= mines.length <= 5000
-0 <= xi, yi < n
-每一对 (xi, yi) 都 不重复​​​​​​​
+0 <= xi, yi< n
+每一对(xi, yi)都 不重复​​​​​​​
 通过次数6,089提交次数12,185
 
 来源：力扣（LeetCode）
 链接：https://leetcode.cn/problems/largest-plus-sign
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-class Solution {
+class Solution
+{
 public:
-    int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
-        return 1;
+    int orderOfLargestPlusSign(int n, vector<vector<int>> &mines)
+    {
+        vector<vector<int>> grid(n, vector<int>(n, n));
+        unordered_set<int> banned;
+        for (int i = 0; i < mines.size(); i++)
+        {
+            banned.emplace(mines[i][0] * n + mines[i][1]);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (banned.count(i * n + j))
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+                grid[i][j] = min(grid[i][j], count);
+            }
+            count = 0;
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (banned.count(i * n + j))
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+                grid[i][j] = min(grid[i][j], count);
+            }
+        }
+        print_mat(grid);
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (banned.count(j * n + i))
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+                grid[j][i] = min(grid[j][i], count);
+            }
+            count = 0;
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (banned.count(j * n + i))
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+                grid[j][i] = min(grid[j][i], count);
+                res = max(res, grid[j][i]);
+            }
+        }
+        return res;
     }
 };
+
+
+void TestSolution() {
+    Solution s;
+    vector<vector<int>> mines = {
+        {0, 1},
+    };
+    s.orderOfLargestPlusSign(3, mines);
+}
