c656ddb3d9ee52b9f6f7513903b1ae46c35e4474
chenugray <chenugray@outlook.com>
2022-11-09 23:37:56 +0800

    update 0198

--- leetcode/0198_house-robber.cpp
+++ leetcode/0198_house-robber.cpp
@@ -34,13 +34,13 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2) return max(nums[0], nums[1]);
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int p = nums[0];
+        int q = max(nums[0], nums[1]);
         for(int i = 2; i < nums.size(); i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            int s = max(q, p + nums[i]);
+            p = q;
+            q = s;
         }
-        return dp.back();
+        return q;
     }
 };
