c443fb0e90fef67d8280d42b6283315f687b8bef
chenugray <chenugray@outlook.com>
2022-11-12 22:11:37 +0800

    update 0092

--- leetcode/0092_reverse-linked-list-ii.cpp
+++ leetcode/0092_reverse-linked-list-ii.cpp
@@ -1,46 +1,31 @@
 #include "common.h"
 
-class Solution
-{
+
+class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int left, int right)
-    {
-        ListNode *tmp = new ListNode(INT_MAX);
-        tmp->next = head;
-        ListNode *l1 = head;
-        ListNode *l2 = head;
-        bool is_change = true;
-        function<void(ListNode *&, ListNode *)> dfs = [&](ListNode *&lhs, ListNode *rhs)
-        {
-            if (rhs->next->val != right)
-            {
-                if (lhs->next->val != left)
-                {
-                    dfs(lhs->next, rhs->next);
-                }
-                else
-                {
-                    dfs(lhs, rhs->next);
-                }
-            }
-            if (is_change)
-            {
-                if (lhs == rhs) {
-                    is_change = false;
-                    return;
-                } 
-                ListNode *tmp = lhs->next;
-                lhs->next = rhs->next;
-                rhs->next = tmp;
-                tmp = lhs->next->next;
-                lhs->next->next = rhs->next->next;
-                rhs->next->next = tmp;
-                if (lhs->next == rhs) {
-                    is_change = false;
-                }
-            }
-        };
-        dfs(l1, l2);
-        return tmp->next;
+    ListNode *reverseBetween(ListNode *head, int left, int right) {
+        ListNode *dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode *pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre->next;
+        }
+        ListNode *cur = pre->next;
+        ListNode *next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur->next;
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+        }
+        return dummyNode->next;
     }
+
 };
+
+void TestSolution() {
+    Solution s;
+    ListNode* head = make_list_node({1, 2, 3, 4, 5});
+    auto res = s.reverseBetween(head, 2, 3);
+    print_list_node(res);
+}
