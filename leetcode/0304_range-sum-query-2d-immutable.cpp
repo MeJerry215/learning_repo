fa633ad784f2313138931347b03673c88c8951c3
chenugray <chenugray@outlook.com>
2022-11-13 19:22:04 +0800

    update 0304

--- leetcode/0304_range-sum-query-2d-immutable.cpp
+++ leetcode/0304_range-sum-query-2d-immutable.cpp
@@ -13,15 +13,15 @@ public:
         }
         for(int i = 0; i < matrix.size(); i ++) {
             for(int j = 0; j < matrix[0].size(); j++) {
-                sum_matrix[i + 1][j + 1] = matrix[i][j] + sum_matrix[i, j + 1] + sum_matrix[i + 1, j] - sum_matrix[i, j];
+                sum_matrix[i + 1][j + 1] = matrix[i][j] + sum_matrix[i][j + 1] + sum_matrix[i + 1][j] - sum_matrix[i][j];
             }
         }
-        print_mat(sum_matrix);
+        // print_mat(sum_matrix);
 
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum_matrix[row2 + 1][col2 + 1] - sum_matrix[row1][col2 + 1] - sum_matrix[row2 + 1][col1] + P[row1][col1];
+        return sum_matrix[row2 + 1][col2 + 1] - sum_matrix[row1][col2 + 1] - sum_matrix[row2 + 1][col1] + sum_matrix[row1][col1];
     }
 };
 
