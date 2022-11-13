45fdf74d7d40784d7125665a1d2638187a49f81c
chenugray <chenugray@outlook.com>
2022-11-13 20:19:56 +0800

    update 1109

---
+++ leetcode/1109_corporate-flight-bookings.cpp
@@ -0,0 +1,26 @@
+#include "common.h"
+
+
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> nums(n);
+        for(auto& booking: bookings) {
+            nums[booking[0] - 1] += booking[2];
+            if (booking[1] < n)
+            nums[booking[1]] -= booking[2];
+        }
+        for(int i = 1; i < n; i++) {
+            nums[i] += nums[i];
+        }
+        return nums;
+    }
+};
