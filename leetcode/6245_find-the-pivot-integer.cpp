2870e95396b5c2410c3d89cd06cc9bf597f4e8a6
chenugray <chenugray@outlook.com>
2022-11-27 18:43:06 +0800

    update 6245, one word æ: 数学

---
+++ leetcode/6245_find-the-pivot-integer.cpp
@@ -0,0 +1,11 @@
+#include "common.h"
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int m = n * (n + 1) / 2;
+        int x = sqrt(m);
+        return x * x == m ? x: -1; 
+    }
+};
+
